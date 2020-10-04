ff422212b0e831609609ea8cce605f11cb8797be
phamtruong2k1 <61191253+phamtruong2k1@users.noreply.github.com>
2020-10-04 21:44:56 +0700

    Update bai2.java

--- bai2.java
+++ bai2.java
@@ -1,25 +1,25 @@
-public class baitap2 {
+import java.util.Scanner;
+
+public class baitapbuoi2 {
     public static void main(String[] args) {
-        System.out.println("*       *     *     * * * *     * * * *    *     *");
-        System.out.println("|       |    / \\    |       *   |       *   \\   /");
-        System.out.println("* ----- *   *---*   * * * *     * * * *       * ");
-        System.out.println("|       |  /     \\  |           |             |");
-        System.out.println("*       * *       * *           *             *");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println("*   *  ****** *       *       *");
-        System.out.println("|\\  |  |       \\     / \\     /");
-        System.out.println("* * *  ******   *   *   *   *");
-        System.out.println("|  \\|  |         \\ /     \\ /");
-        System.out.println("*   *  ******     *       *");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println("*     * ******     *     * * * *");
-        System.out.println(" \\   /  |         / \\    |       *");
-        System.out.println("   *    ******   *---*   * * * *");
-        System.out.println("   |    |       /     \\  |    \\ ");
-        System.out.println("   *    ****** *       * *     *");
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] a = new int[1000];
+        for (int i=1;i<=n;i++)
+            a[i]=sc.nextInt();
+        for (int i=1;i<n;i++)
+            for (int j=i+1;j<=n;j++)
+                if (a[i]>a[j]){
+                    int tg;
+                    tg=a[i];
+                    a[i]=a[j];
+                    a[j]=tg;
+                }
+        System.out.println("Phan tu be nhat:" + a[1]);
+        System.out.println("Phan tu lon nhat:" + a[n]);
+        System.out.println("Mang xep tang:");
+        for (int i=1;i<=n;i++)
+            System.out.print(a[i]+" ");
     }
 }
+
