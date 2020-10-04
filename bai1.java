64ee6660e242fc149746b28c48850444ce1ca39d
phamtruong2k1 <61191253+phamtruong2k1@users.noreply.github.com>
2020-10-04 21:37:41 +0700

    Update bai1.java

--- bai1.java
+++ bai1.java
@@ -1,22 +1,15 @@
-package com.company;
 import java.util.Scanner;
-public class baitap1 {
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            int n = input.nextInt();
-            int m = input.nextInt();
-            for (int i = 1; i <= n; ++i)
-                if(i==1 || i==n) {
-                    for (int j = 1; j <= m; ++j)
-                        System.out.print("* ");
-                    System.out.println();
-                }
-                else {
-                    for(int j=1; j<=m; ++j)
-                        if(j==1||j==m)
-                            System.out.print("* ");
-                        else System.out.print("  ");
-                    System.out.println();
-                }
-        }
+
+public class baitapbuoi2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+        int ans;
+        ans=a;
+        if (ans<b) ans=b;
+        if (ans<c) ans=c;
+        System.out.print("Ket qua:" + ans);
+    }
 }
